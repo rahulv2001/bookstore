edf0c4f153310ceacf73b5d001804637d8af54e3
Rahul Vishwakarma <rahulsaidupur232103@gmail.com>
2023-11-28 20:27:13 +0530

    API_URL fixed

--- client/src/components/Book/Api.js
+++ client/src/components/Book/Api.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8000/books";
-// export const API_URL = "https://book-app-x0rn.onrender.com/books";
+// export const API_URL = "http://localhost:8000/books";
+export const API_URL = "https://book-app-x0rn.onrender.com/books";
